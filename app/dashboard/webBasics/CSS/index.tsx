d375ea7731d57e5c50e2e9a56b70a1ae83e9e041
bastuce <bastuce.commit@gmail.com>
2025-05-17 23:52:10 +0200

    Starting creation of my VeryBeginner site for write knowledge and share it.

---
+++ app/dashboard/webBasics/CSS/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function CSS() {
+  return (
+    <>
+    <div>CSS</div>
+    <div className="w3-boxmodel">
+        <div className="margin">
+            <div className="border w3-theme">
+                <div className="padding">
+                    <div className="content"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    layout("dashboard/index.tsx",[
+        route("HTML","dashboard/webBasics/HTML/index.tsx"),
+        route("CSS","dashboard/webBasics/CSS/index.tsx"),
+        route("Javascript","dashboard/webBasics/Javascript/index.tsx"),
+    ])] satisfies RouteConfig;
